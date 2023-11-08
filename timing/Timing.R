86a2c00c32d598e5abe6d93c142683107d6ce504
ING-HGA <45300713+ING-HGA@users.noreply.github.com>
2023-11-08 16:45:50 -0600

    Update Timing.R
    Add MacOS timing

--- timing/Timing.R
+++ timing/Timing.R
@@ -24,4 +24,5 @@ data_dir <- "../../julia_example-data/imzML_AP_SMALDI"
 fileName <- "HR2MSImouseurinarybladderS096.imzML"
 time     <- LoadingTime( file.path( data_dir, fileName ) )
 
-# Ubuntu 23.04 16GB, Intel® Celeron® N5105 × 4 [1] 1.284737 1.241130 1.212906 1.251302 1.285321 1.288718 1.329186 1.317943 1.452790 1.275222
+# Ubuntu 23.04 16GB, Intel® Celeron® @2.0 GHz N5105 × 4 [1] 1.284737 1.241130 1.212906 1.251302 1.285321 1.288718 1.329186 1.317943 1.452790 1.275222
+# MacOS 13.6.1 16GB, Intel® i7 7820HQ @2.9 GHz x 4 SSD up to 6gb/s  [1] 1.364117 1.301740 1.255954 1.201883 1.157282 1.163385 1.181011 1.155240 1.147595 1.171399
